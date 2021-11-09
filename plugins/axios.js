dcb903ce4d1c3ae92598ae200518b103ad1daf47
Xitão Moura <washington0moura@gmail.com>
2021-11-09 08:33:06 -0300

    feat: update baseurl

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,6 @@
 export default function({ $axios }, inject) {
     const api = $axios.create({
-        baseURL: 'https://api.kvsh.com.br/'
+        baseURL: 'http://3.86.49.73:4000/'
     })
     inject('api', api)
 }
