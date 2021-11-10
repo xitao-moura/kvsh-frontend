c7e59150eefe05ead06ad1b549f7feef51de8ffc
Xitão Moura <washington0moura@gmail.com>
2021-11-09 22:38:42 -0300

    update

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,6 @@
 export default function({ $axios }, inject) {
     const api = $axios.create({
-        baseURL: 'http://3.86.49.73:4000/'
+        baseURL: 'https://kvsh-api.musiclounge.com.br'
     })
     inject('api', api)
 }
