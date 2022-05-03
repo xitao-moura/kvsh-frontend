ea275203cf390d4c1e56e4310088bf1ce038784a
Xitão Moura <xitaomoura@Xitaos-iMac-Pro.local>
2022-05-03 03:06:33 -0300

    feat: add page tracks and about

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: 'KVSH Frontend',
+        name: 'KVSH Frontend Homolog',
         exec_mode: 'fork',
         //instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
